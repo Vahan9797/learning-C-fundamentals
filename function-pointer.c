007ddac1d9816dc12016a7c50df60c3a8e6b06e6
Vahan Arakelyan <vahan.arakelyan@betconstruct.com>
2018-06-07 18:42:38 +0400

    Minor fixes in function-pointer

--- function-pointer.c
+++ function-pointer.c
@@ -60,11 +60,11 @@ int main(void) {
     unsigned int ch, a = 15, b = 10;
 
     printf("Enter Choice: 0 for add, 1 for subtract and 2 for multiply: ");
-    scanf("%d\n", &ch);
+    scanf("%d", &ch);
 
     if(ch > 2) return 0;
 
-    (*fun_ptr_arr)[ch](a, b);
+    (*fun_ptr_arr[ch])(a, b);
 
     return 0;
 }
