05c7b19a05ee4077131002a932f0620a4734b7b0
Vahan9797 <vahan9797@gmail.com>
2018-06-16 14:33:41 +0400

    ADD new check in compiler.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Add every specific linkages with libs inside this case
-linkingLibs="";
 
 linking_lib() {
+    linkingLibs="";
+
     case "$1" in
         pthreads.c)
             linkingLibs="-pthread"
@@ -13,25 +14,30 @@ linking_lib() {
             ;;
         #TODO: make cases for every file
     esac
+
+    echo ${linkingLibs}
 }
+
+#check if "source-files" directory not exists and create it
+if [ ! -d "./source-files" ]; then
+    mkdir ./source-files
+fi
 
 # destination should be source-files folder
 if [ $# -eq 0 ]; then
 	for name in *.c; do
-        linking_lib ${name};
 	    echo "Compiling \033[0;33m$name\033[0m ..."
-        gcc ${linkingLibs} ${name} -o ./source-files/"${name%.*}" && echo "\033[1;92m$name\033[0m done."
+        gcc $(linking_lib ${name}) ${name} -o ./source-files/"${name%.*}" && echo "\033[1;92m$name\033[0m done."
 	done
 else
     for name in "$@"; do
-        linking_lib ${name};
         echo "Compiling \033[0;33m$name\033[0m ..."
         case "$name" in
             *.c)
-                gcc ${linkingLibs} $(basename "$name") -o ./source-files/"${name%.*}" && echo "\033[1;92m$name\033[0m done."
+                gcc $(linking_lib ${name}) $(basename "$name") -o ./source-files/"${name%.*}" && echo "\033[1;92m$name\033[0m done."
                 ;;
             *.cc)
-                g++ ${linkingLibs} $(basename "$name") -o ./source-files/"${name%.*}" && echo "\033[1;92m$name\033[0m done."
+                g++ $(linking_lib ${name}) $(basename "$name") -o ./source-files/"${name%.*}" && echo "\033[1;92m$name\033[0m done."
                 ;;
             *)
                 echo "Ignoring $name, since it is not one of my files!"
